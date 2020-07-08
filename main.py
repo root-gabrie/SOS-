a3d9f910352df3fbd0205959daf56c3f01585ad6
root <root@root.gabriel>
2020-07-08 23:00:01 +0300

    SOS v 1.1

--- main.py
+++ main.py
@@ -6,62 +6,67 @@ from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-
 import imaplib
 import getpass
 
-
-#import simpleaudio as sa
+import winsound
 
 #####################################################################################################
 
-Resault=True
+Resault=0
+
+def MAGIC(chislo):
+    global Resault
+
+    if chislo == 0:
+        Resault=True
+    else:
+        Resault=False
+    return Resault
 
 #####################################################################################################
 
 def new_message():
-	filename = 'new_message.wav'
 
-	wave_obj = sa.WaveObject.from_wave_file(filename)
-	play_obj = wave_obj.play()
-	play_obj.wait_done()
+    winsound.PlaySound('new_message.wav', winsound.SND_FILENAME)
 
-	return False
+    return False
 
 def start_check(self):
-	imap = imaplib.IMAP4_SSL('imap.yandex.ru')
-	imap.login('support.l1@gcib.ru', 'j2Awgdj7Hj5X')
+    imap = imaplib.IMAP4_SSL('imap.yandex.ru')
+    imap.login('support.l1@gcib.ru', 'j2Awgdj7Hj5X')
 
-	imap.list()
-	itog_message = imap.select('INBOX')
+    new_message()
 
-	now_message=itog_message
+    imap.list()
+    itog_message = imap.select('INBOX')
 
-	if itog_message == now_message:
-		now_message=imap.select('INBOX')
-		print ("1")
-		return Resault
-	else:
-		new_message()
+    now_message=itog_message
 
+    if itog_message == now_message:
+        now_message=imap.select('INBOX')
+        return Resault
+    else:
+        new_message()
 
 #####################################################################################################
 
 class ScreenManagement(ScreenManager):
-	pass
+    pass
 
 class Start_Programm_SOS(Screen):
-	def start_chk(self):
-		Clock.schedule_interval(start_check,2)
-	def stop_check(self):
-		Resault=False
-	def exit(self):
-		App.get_running_app().stop()
+    global Resault
+    def start_chk(self):
+        Clock.schedule_interval(start_check,2)
+    def stop_check(self):
+        MAGIC(1)
+    def exit(self):
+        App.get_running_app().stop()
 
 class SOSApp(App):
-	def build(self):
-		Window.clearcolor = (1, 1, 1, 1)
-		return ScreenManagement()
+    def build(self):
+        Window.clearcolor = (1, 1, 1, 1)
+        return ScreenManagement()
 
 if __name__=="__main__":
-	SOSApp().run()
+    SOSApp().run()
